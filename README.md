3b337e63261f5fca5149c34ca9e24a282a15ad1e
haikal.rahman <haikal.rahman@ui.ac.id>
2024-02-26 20:31:57 +0700

    Implement crouch, sound, and other changes
    Changes include movement, animation, debug mode, etc.

---
+++ README.md
@@ -0,0 +1,39 @@
+> Haikal Rahman - 200697891 <br>
+> Game Development <br>
+> Tutorial 3 <br>
+
+
+# Latihan: Pergerakan Horizontal Menggunakan Script
+Pergerakan horizontal dilakukan di dalam fungsi `get_input()`. Pergerakan ini dipisah dalam berbagai conditional `if` yang menangkap input. Satu pergerakan horizontal juga, conditional `if` tersebut juga dibagi menjadi conditional yang mengatur pergerakan dan yang mengatur animasi.
+
+### Inisialisasi Variabel
+Variabel-variabel awal ini secara konsisten diubah dan ditambahkan bergantung keperluan.
+![Inisialisasi Variabel](./markdown-assets/init-variable.png)
+
+Variabel ini termasuk speed, dash_intensity, jump_speed, GRAVITY, dan lain sebagainya yang menentukan *kehalusan (smoothnes)* pergerakan. 
+
+Bukan hanya itu, terdapat juga variabel waktu, seperti DASH_DELAY dan DASH_COOLDOWN, yang menentukan interval waktu *double-tap*, serta *cooldown* dari dash itu sendiri.
+
+### Pergerakan Horizontal
+![Kode Pergerakan Horizontal](./markdown-assets/horizontal-movement.png)<br>
+Bagian Kode ini merupakan bagian kode pergerakan ke kanan dan kiri. Pada bagian kode tersebut, terdapat perintah untuk membalik posisi *sprite* untuk mengarah ke arah pergerakan.
+
+![Kode Animasi Horizontal dan Idle](./markdown-assets/horizontal-animation.png)<br>
+Bagian ini menenttukan animasi pergerakan horizontal, termasuk pergerakan idle jika tombol kanan dan kiri tidak ditekan. Perlu diketahui bahwa animasi ini hanya dijalankan jika *player* berada di lantai atau di suatu platform.
+
+### Pergerakan Vertikal (*Jump*, *Double Jump*, dan *Crouch*)
+![Kode Jump dan Double Jump](./markdown-assets/jump.png)<br>
+Kode ini merepresentasikan kemampuan *player* untuk melakukan *jump* dan *double jump*. Jump pertama hanya dapat dilakukan jika player berada di lantai atau platform. *Jump* kedua dapat dilakukan ketika *player* berada di udara. Kondisi tersebut direpresentasikan dalam tiga tipe `jump_state` yang sudah tertulis pada komentar tersebut. 
+
+![Kode Crouch](./markdown-assets/crouch.png)<br>
+Implementasi *crouch* sendiri dilakukan jika player berada pada lantai atau *platform*. Jika tombol *crouch* ditekan atau ditahan, animasi *crouch* dijalankan (selagi tidak berada di atas udara, yang mana animasi jump dijalankan) dan *movement speed* nya dikurangi menjadi `0.2`. 
+
+> Kurang lebih, animasi pergerakan *player*, didesain untuk memiliki prioritas tertentu, yaitu *jump* atau *double jump*, lalu *crouch*, lalu *walk*, lalu *idle*.
+
+# Latihan: Implementasi Physics Sederhana
+![Implementasi Physics](./markdown-assets/physics.png)<br>
+Implementasi *physics* dilakukan kurang lebih sama dengan apa yang dicontohkan oleh tutorial (gravitasi, pengambilan input, dan pergerakan dengan *collision*). Tambahan dilakukan pada `dash_timeout` dan `dash_cd_timeout` yang merupakan hitung mundur *double tap* (hitung mundur tap yang terhitung sebagai *double tap*) dan *dash* (hitung mundur untuk *cooldown* antar-dash agar tidak dispam.)
+
+# Latihan: Implementasi Sound Effect
+![Implementasi Sound Effect](./markdown-assets/audio.png)<br>
+Implementasi Sound Effect dilakukan dengan menambahkan objek `AudioStreamPlayer`.
--- scenes/Player.gd
+++ scenes/Player.gd
@@ -8,8 +8,12 @@ extends KinematicBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("Welcome!") # Replace with function body.
-
+	print("Welcome to the game")
+	print("Press A or D to move left/right.")
+	print("Press ctrl to crouch.")
+	print("Press space to jump to crouch.")
+	print("All assets and sound effects belong to their respective owners.")
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 export (int) var speed = 400
@@ -23,45 +27,76 @@ export (int) var DASH_COOLDOWN = 1
 export (int) var dash_timeout = DASH_DELAY #doubletap timeout
 export (int) var dash_cd_timeout = DASH_COOLDOWN
 export       var last_key = ""
+export       var DEBUG = false
+export (int) var speed_multiplier = 1
 
 var velocity = Vector2()
 
 func get_input():
 	velocity.x = 0
 	var dash = 0
-	$AnimatedSprite.animation = "default"
 	if ((Input.is_action_just_pressed("right") and last_key == "right") or (Input.is_action_just_pressed("left") and last_key == "left")) and dash_timeout >= 0 and dash_cd_timeout <= 0: 
 		dash = dash_intensity
 		last_key = ""
 		dash_cd_timeout = DASH_COOLDOWN
-		print("just dashed, now cooldown " + str(dash_cd_timeout))
+		if DEBUG: print("just dashed, now cooldown " + str(dash_cd_timeout))
 		
-	
 	if Input.is_action_just_pressed("right"):
 		last_key = "right"
 		dash_timeout = DASH_DELAY
-		print("last_key right, cd timeout " + str(dash_cd_timeout))
+		$AudioStreamPlayer.play(0.96)
+		if DEBUG:print("last_key right, cd timeout " + str(dash_cd_timeout))
 
 	if Input.is_action_just_pressed("left"):
 		last_key = "left"
 		dash_timeout = DASH_DELAY
-		print("last_key left, cd timeout " + str(dash_cd_timeout))
+		$AudioStreamPlayer.play(0.96)
+		if DEBUG: print("last_key left, cd timeout " + str(dash_cd_timeout))
 		
 	if Input.is_action_pressed('right'):
-		velocity.x += speed + dash
-		$AnimatedSprite.animation = "Running"
+		velocity.x += (speed)*speed_multiplier + dash
+		$AnimatedSprite.flip_h = false
 		
 	if Input.is_action_pressed('left'):
-		velocity.x -= speed + dash
-		$AnimatedSprite.animation = "Running"
-	if is_on_floor() and Input.is_action_just_pressed('jump'):
-		velocity.y = jump_speed
-		jump_state = 1
-		print(jump_state)
-	if !is_on_floor() and jump_state == 1 and Input.is_action_just_pressed('jump'):
-		velocity.y = jump_speed
+		$AnimatedSprite.flip_h = true
+		velocity.x -= (speed)*speed_multiplier + dash
+	
+	if is_on_floor(): 
+		# running animation
+		if Input.is_action_pressed("left") or Input.is_action_pressed("right"):
+			if $AnimatedSprite.animation != 'run': $AnimatedSprite.play('run') #prevent continuous replay of animation
+		
+		# idle animation
+		else:
+			$AnimatedSprite.play('default')
+			if DEBUG: print('Animation changed to: ' + $AnimatedSprite.animation)		
+
+	# jump
+	if !is_on_floor():
+		if jump_state == 0: jump_state = 2 
+		# 0 is on floor, then 2 on air, 1 already double jump
+	else:
 		jump_state = 0
-		print(jump_state)
+	if Input.is_action_just_pressed('jump'):
+		if is_on_floor(): 
+			velocity.y = jump_speed
+			jump_state = 2 
+			$AnimatedSprite.play("jump")
+			if DEBUG: print('Current jump state is: ' + str(jump_state))
+		if !is_on_floor() and jump_state == 2: #jump_state makes sure they can only double jump once
+			velocity.y = jump_speed
+			jump_state = 1
+			$AnimatedSprite.play("double_jump")
+			if DEBUG: print('Current jump state is: ' + str(jump_state))
+	
+	#crouch
+	if is_on_floor() and Input.is_action_pressed("crouch"):
+		speed_multiplier = 0.2
+		if $AnimatedSprite.animation != 'jump': $AnimatedSprite.play("crouch") #fix crouching after jumping issue (1 frame still on floor)
+		if DEBUG: print('Now crouching')
+	else:
+		speed_multiplier = 1
+		
 
 func _physics_process(delta):
 	velocity.y += delta * GRAVITY
